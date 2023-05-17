b6ff3b9a12b2d26ff3af0eb7243e8bcad906a948
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-17 08:48:41 +0300

    module structure

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { someReducer } from '../modules/module';
 
 export const store = configureStore({
-  resucer: {},
+  reducer: {
+    some: someReducer,
+  },
 });
