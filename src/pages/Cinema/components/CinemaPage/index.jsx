384b4791d5dcfa108c65e4945cc163c3faa804e1
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-18 12:20:59 +0300

    tab ui

--- src/pages/Cinema/components/CinemaPage/index.jsx
+++ src/pages/Cinema/components/CinemaPage/index.jsx
@@ -1,13 +1,26 @@
 import styles from './style.module.scss';
 import globalStyles from '../../../../styles/global.module.scss';
+import Tab from '../../../../ui/Tab';
+import { useEffect, useState } from 'react';
 
 function CinemaPage() {
+  const [defenitionList, setDefenitionList] = useState({ titles: [], contents: [] });
+
+  useEffect(() => {
+    setDefenitionList({
+      titles: ['Прибутки', 'Зали'],
+      contents: [<button>Income</button>, 'Halls'],
+    });
+  }, []);
+
   return (
     <div className={globalStyles.wrapper}>
       <div className={globalStyles.container}>
         <div className={globalStyles.inner}>
           <div className={`${styles.block} ${styles.block_head}`}>Block Title cinema</div>
-          <div className={styles.block}>Block info cinemas</div>
+          <div className={styles.block}>
+            <Tab titles={defenitionList.titles} contents={defenitionList.contents} />
+          </div>
           <div className={styles.block}>Block list sessions</div>
         </div>
       </div>
--- src/ui/Tab/index.jsx
+++ src/ui/Tab/index.jsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
 import styles from './style.module.scss';
 
-function Component2() {
-  return <></>;
+function Tab({ titles, contents }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  function handleClick(id) {
+    setActiveIndex(id);
+  }
+
+  return (
+    <div className={styles.tab}>
+      <nav className={styles.tab_controls}>
+        {titles.map((title, id) => {
+          const _className =
+            styles.tab_link + ' ' + (id === activeIndex ? styles.tab_link__active : '');
+          return (
+            <button key={title} className={_className} onClick={() => handleClick(id)}>
+              {title}
+            </button>
+          );
+        })}
+      </nav>
+      <div className={styles.tab_inner}>
+        {contents.map((content, id) => {
+          const _className =
+            styles.tab_content + ' ' + (id === activeIndex ? styles.tab_content__active : '');
+          return (
+            <div key={id} className={_className}>
+              {content}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Component2;
+export default Tab;
