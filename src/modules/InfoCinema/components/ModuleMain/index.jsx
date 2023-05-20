879a8214ee51e9b932a5209a43e113ab1042915c
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-20 16:51:58 +0300

    info cinema module & overflow table ui

--- src/modules/InfoCinema/components/ModuleMain/index.jsx
+++ src/modules/InfoCinema/components/ModuleMain/index.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react';
 import styles from './style.module.scss';
 
+import Tab from '../../../../ui/Tab';
+import Table from '../../../../ui/Table';
+
 function ModuleMain() {
-  return <></>;
+  const [titles, setTitles] = useState([]);
+  const [contents, setContents] = useState([]);
+
+  useEffect(() => {
+    const titlesIncome = ['Дата', 'Прибуток'];
+    const listIncome = [
+      ['20.09.2023', '1 300 $'],
+      ['22.09.2023', '2 100 $'],
+      ['23.09.2023', '950 $'],
+      ['26.09.2023', '9 150 $'],
+      ['26.09.2023', '3 800 $'],
+    ];
+
+    const titlesHalls = ['№', 'Статус', 'Місць'];
+    const listHalls = [
+      ['1', 'BUDGET', '30'],
+      ['2', 'STANDART', '40'],
+      ['3', 'PREMIUM', '15'],
+    ];
+
+    setContents([
+      <Table style={{ marginTop: '1.5rem' }} titles={titlesIncome} contents={listIncome} />,
+      <Table style={{ marginTop: '1.5rem' }} titles={titlesHalls} contents={listHalls} />,
+    ]);
+
+    setTitles(['Прибутки', 'Зали']);
+  }, []);
+
+  return (
+    <div className={styles.root}>
+      <Tab titles={titles} contents={contents} />
+    </div>
+  );
 }
 
 export default ModuleMain;
--- src/pages/Cinema/components/CinemaPage/index.jsx
+++ src/pages/Cinema/components/CinemaPage/index.jsx
@@ -1,44 +1,17 @@
 import styles from './style.module.scss';
 import globalStyles from '../../../../styles/global.module.scss';
-import Tab from '../../../../ui/Tab';
-import { useEffect, useState } from 'react';
-import Table from '../../../../ui/Table';
+
 import ListSessions from '../../../../modules/ListSessions';
+import InfoCinema from '../../../../modules/InfoCinema';
 
 function CinemaPage() {
-  const [titles, setTitles] = useState([]);
-  const [contents, setContents] = useState([]);
-
-  useEffect(() => {
-    const titlesIncome = ['Дата', 'Прибуток'];
-    const listIncome = [
-      ['20.09.2023', '1 300 $'],
-      ['22.09.2023', '2 100 $'],
-      ['22.09.2023', '950 $'],
-    ];
-
-    const titlesHalls = ['№', 'Статус', 'Місць'];
-    const listHalls = [
-      ['1', 'BUDGET', '30'],
-      ['2', 'STANDART', '40'],
-      ['3', 'PREMIUM', '15'],
-    ];
-
-    setContents([
-      <Table style={{ marginTop: '1.5rem' }} titles={titlesIncome} contents={listIncome} />,
-      <Table style={{ marginTop: '1.5rem' }} titles={titlesHalls} contents={listHalls} />,
-    ]);
-
-    setTitles(['Прибутки', 'Зали']);
-  }, []);
-
   return (
     <div className={globalStyles.wrapper}>
       <div className={globalStyles.container}>
         <div className={globalStyles.inner}>
           <div className={`${styles.block} ${styles.block_head}`}>Block Title cinema</div>
           <div className={styles.block}>
-            <Tab titles={titles} contents={contents} />
+            <InfoCinema />
           </div>
           <div className={styles.block}>
             <ListSessions />
