a637b1e4cde77b621830604de32f15eed37759c2
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-30 19:00:07 +0300

    some

--- src/modules/InfoSession/components/GeneralInfo/index.jsx
+++ src/modules/InfoSession/components/GeneralInfo/index.jsx
@@ -1,18 +1,35 @@
+import Button from '../../../../ui/Button';
 import styles from './style.module.scss';
 
 function GeneralInfo() {
+  function handleBuy() {
+    alert('buy session');
+  }
+
   return (
     <div className={styles.root}>
       <dl className={styles.list}>
-        <dt className={styles.list_title}>Зали:</dt>
-        <dd className={styles.list_data}>1</dd>
+        <div className={styles.list_item}>
+          <dt className={styles.list_title}>Зала:</dt>
+          <dd className={styles.list_data}>1</dd>
+        </div>
 
-        <dt className={styles.list_title}>Місця:</dt>
-        <dd className={styles.list_data}>5 / 30</dd>
+        <div className={styles.list_item}>
+          <dt className={styles.list_title}>Місця:</dt>
+          <dd className={styles.list_data}>5 / 30</dd>
+        </div>
 
-        <dt className={styles.list_title}>Ціна:</dt>
-        <dd className={styles.list_data}>12 $</dd>
+        <div className={styles.list_item}>
+          <dt className={styles.list_title}>Ціна:</dt>
+          <dd className={styles.list_data}>
+            <b>12 $</b>
+          </dd>
+        </div>
       </dl>
+
+      <Button className={styles.button_buy} handleClick={handleBuy}>
+        Купити
+      </Button>
     </div>
   );
 }
